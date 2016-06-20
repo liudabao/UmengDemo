e2e0b4f551dd05ca660ebeb1963036271478df84
liudabao <15810163587@163.com>
2016-06-20 20:52:37 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/umengdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.administrator.umengdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/umengdemo/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.umengdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+}
